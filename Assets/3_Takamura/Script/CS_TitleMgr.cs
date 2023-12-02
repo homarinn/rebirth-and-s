9ba5fc9157b31447444c1a04c2e3c3bc41e0ec67
tea-tkmr <104489583+tea-tkmr@users.noreply.github.com>
2023-12-02 22:22:53 +0900

    Takamura初コミ

---
+++ Assets/3_Takamura/Script/CS_TitleMgr.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CS_TitleMgr : MonoBehaviour
+{
+    //! @brief ステートマシン
+    enum eState
+    { 
+        None,
+        FadeHide,   //! フェードが明ける
+        Standby,    //! 待機状態
+        FadeShow,   //! フェードが掛かる
+    }
+    eState state;
+
+    [SerializeField]
+    [Header("FadeImage")]
+    CanvasGroup cgFade;
+    [SerializeField]
+    [Header("フェード時間(秒)")]
+    float fadeSpeed = 1.5f;
+
+    //! @brief ステートの変更
+    //! @param nextstate:変更予定のステート
+    void ChangeState(eState nextState)
+    {
+        switch (state)
+        {
+            case eState.FadeHide:
+                cgFade.blocksRaycasts = false;
+                cgFade.interactable = false;
+                break;
+            case eState.Standby:
+                break;
+            case eState.FadeShow:
+                break;
+        }
+
+        switch (nextState)
+        {
+            case eState.FadeHide:
+                break;
+            case eState.Standby:
+                break;
+            case eState.FadeShow:
+                cgFade.blocksRaycasts = true;
+                cgFade.interactable = true;
+                break;
+        }
+
+        state = nextState;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = eState.FadeHide;
+
+        cgFade.alpha = 1.0f;
+        cgFade.blocksRaycasts = true;
+        cgFade.interactable = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case eState.FadeHide:
+                cgFade.alpha -= Time.deltaTime / fadeSpeed;
+                //! Fade終了
+                if (cgFade.alpha <= 0.0f)
+                {
+                    ChangeState(eState.Standby);
+                }
+                break;
+            case eState.Standby:
+                break;
+            case eState.FadeShow:
+                cgFade.alpha += Time.deltaTime / fadeSpeed;
+                if (cgFade.alpha >= 1.0f)
+                {
+                    SceneManager.LoadScene("Stage01Scene");
+                }
+                break;
+        }
+    }
+
+    //! Startボタンが押された時の処理
+    public void OnClickStart()
+    {
+        ChangeState(eState.FadeShow);
+    }
+    //! Quitボタンが押された時の処理
+    public void OnClickQuit()
+    {
+        //! アプリケーション終了
+        Application.Quit();
+    }
+}
