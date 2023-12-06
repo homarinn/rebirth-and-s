8c737b2579de26c011142fc03b6018feb260c806
tea-tkmr <104489583+tea-tkmr@users.noreply.github.com>
2023-12-07 00:25:57 +0900

    UI Texture差し込み

--- Assets/3_Takamura/Script/CS_UIControl.cs
+++ Assets/3_Takamura/Script/CS_UIControl.cs
@@ -11,17 +11,8 @@ public class CS_UIControl : MonoBehaviour
     [Header("PlayerHPバー")]
     Image playerHP;
     [SerializeField]
-    [Header("必殺技アイコン")]
+    [Header("必殺技バー")]
     Image specialMove;
-    [SerializeField]
-    [Header("必殺技マスクアイコン")]
-    Image spMask;
-
-    //[SerializeField]
-    //CS_Spirit csSpirit;
-    [SerializeField]
-    [Header("精霊回復バー")]
-    Image spilit;
 
     //[SerializeField]
     //CS_Enemy csEnemy;
@@ -37,28 +28,28 @@ public class CS_UIControl : MonoBehaviour
     void Start()
     {
         //! Todo:各キャラのステータスの初期化
-        spilit.fillAmount = 0.0f;
+        specialMove.fillAmount = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        ControlPlayerHP();
-        ControlSpilit();
-        ControlSpecilMove();
 
-        ControlEnemyHP();
+        ControlPlayerHPBar();
+        ControlSpecilMoveBar();
+
+        ControlEnemyHPBar();
 
         //! test
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            currentTime = cooldownTime;
+            specialMove.fillAmount = 0.0f;
         }
     }
 
 
     //! @brief PlayerHPバー操作
-    void ControlPlayerHP()
+    void ControlPlayerHPBar()
     {
         //! Todo:PlayerScriptからHP取得(割合に変換して代入)
         //playerHP.fillAmount -= 0.001f;
@@ -67,33 +58,19 @@ public class CS_UIControl : MonoBehaviour
             playerHP.fillAmount = 0.0f;
         }
     }
-    //! @brief Player必殺技アイコン操作
-    void ControlSpecilMove()
-    {
-        if (currentTime <= 0.0f)
-        {
-            spMask.fillAmount = 0.0f;
-        }
-        else
-        {
-            currentTime -= Time.deltaTime;
-            float val = Mathf.Clamp01(currentTime / cooldownTime);
-            spMask.fillAmount = val;
-        }
-    }
     //! @brief 精霊回復バー操作
-    void ControlSpilit()
+    void ControlSpecilMoveBar()
     {
         //! Todo:SpilitScriptからゲージ取得(割合に変換して代入)
-        spilit.fillAmount += 0.001f;
-        if (spilit.fillAmount >= 1.0f)
+        //specialMove.fillAmount += 0.001f;
+        if (specialMove.fillAmount >= 1.0f)
         {
-            spilit.fillAmount = 1.0f;
+            specialMove.fillAmount = 1.0f;
         }
     }
 
     //! @brief EnemyHPバー操作
-    void ControlEnemyHP()
+    void ControlEnemyHPBar()
     {
         //! Todo:EnemyScriptからHP取得(割合に変換して代入)
         //enemyHP.fillAmount -= 0.0001f;
