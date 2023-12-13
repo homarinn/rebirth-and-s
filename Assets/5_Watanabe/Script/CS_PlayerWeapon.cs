cf54e35e6045df8ffadc447eeb589728499828de
Midori <watanabemidori417@gmail.com>
2023-12-13 18:55:32 +0900

    Player修正

--- Assets/5_Watanabe/Script/CS_PlayerWeapon.cs
+++ Assets/5_Watanabe/Script/CS_PlayerWeapon.cs
@@ -5,9 +5,30 @@ using UnityEngine;
 // Player武器スクリプト
 public class CS_PlayerWeapon : MonoBehaviour
 {
+    [SerializeField, Header("攻撃空振りSE")]
+    private AudioClip SE_PlayerAttackMis;
+    [SerializeField, Header("攻撃１ヒットSE")]
+    private AudioClip SE_PlayerAttack1Hit;
+    [SerializeField, Header("攻撃2ヒットSE")]
+    private AudioClip SE_PlayerAttack2Hit;
+
+    private CS_Player cs_Player;
+
+    private float attack1Power;
+    private float attack2Power;
+
+
+    private void Start()
+    {
+        // コンポーネント取得
+        cs_Player = GetComponentInParent<CS_Player>();
+        attack1Power = cs_Player.Attack1Power;
+        attack2Power = cs_Player.Attack2Power;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        float damage = GetComponentInParent<CS_Player>().GetDamage;
+        float damage = cs_Player.GetDamage;
         if (other.gameObject.tag == "Enemy")
         {
             if (other.GetComponent<CS_Titan>() != null)
@@ -18,7 +39,20 @@ public class CS_PlayerWeapon : MonoBehaviour
             {
                 Debug.Log("タイタンのコンポーネントがないよ");
             }
+
+            if (cs_Player.GetComponent<AudioSource>() != null)
+            {
+                if (damage == attack1Power)
+                {
+                    cs_Player.GetComponent<AudioSource>().PlayOneShot(SE_PlayerAttack1Hit);
+                }
+                else if (damage == attack2Power)
+                {
+                    cs_Player.GetComponent<AudioSource>().PlayOneShot(SE_PlayerAttack2Hit);
+                }
+            }
         }
+    
         // 弱点に衝突したら弱点ダメージを与える
         if(other.gameObject.tag == "EnemyWeakness")
         {
@@ -30,6 +64,18 @@ public class CS_PlayerWeapon : MonoBehaviour
             {
                 Debug.Log("タイタンのコンポーネントがないよ");
             }
+            if (cs_Player.GetComponent<AudioSource>() != null)
+            {
+                if (damage == attack1Power)
+                {
+                    cs_Player.GetComponent<AudioSource>().PlayOneShot(SE_PlayerAttack1Hit);
+                }
+                else if (damage == attack2Power)
+                {
+                    cs_Player.GetComponent<AudioSource>().PlayOneShot(SE_PlayerAttack2Hit);
+                }
+            }
+
         }
     }
 }
