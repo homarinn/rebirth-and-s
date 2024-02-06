84d854b261330cc3012558d8dff841538f004d45
tea-tkmr <104489583+tea-tkmr@users.noreply.github.com>
2024-02-06 15:18:22 +0900

    タイトルシーン画像差し替え

--- Assets/3_Takamura/Script/CS_ArrowControl.cs
+++ Assets/3_Takamura/Script/CS_ArrowControl.cs
@@ -12,9 +12,11 @@ public class CS_ArrowControl : MonoBehaviour,IPointerEnterHandler,IPointerExitHa
     public void OnPointerEnter(PointerEventData eventData)
     {
         var pos = this.transform.position;
-        pos = new Vector3(-270.0f, pos.y);
-        goArrow.transform.position = this.transform.position;
+        Debug.Log(pos);
+        pos = new Vector3(-270.0f, pos.y, 0.0f);
+        Debug.Log(pos);
         goArrow.SetActive(true);
+        goArrow.transform.position = pos;
     }
 
     public void OnPointerExit(PointerEventData eventData)
