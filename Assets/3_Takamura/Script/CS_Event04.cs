cc3886647568fd039da0c6ff74927669183db9c4
tea-tkmr <104489583+tea-tkmr@users.noreply.github.com>
2024-02-23 00:37:55 +0900

    イベントシーン04中間地点までのカメラワーク

---
+++ Assets/3_Takamura/Script/CS_Event04.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Event04 : MonoBehaviour
+{
+    Animator anim;
+    [SerializeField, Header("dialogueのオブジェクト")]
+    GameObject goDialogue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int index = goDialogue.GetComponent<CS_Dialogue>().TextIndex;
+        if (index == 3)
+        {
+            anim.SetBool("bMove04", true);
+        }
+    }
+}
